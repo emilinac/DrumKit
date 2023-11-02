18a46e7b62ed2f7d959208e080718e4d32e60cc5
emilinasanduleac <e.codreanu@students.clark.edu>
2023-11-02 12:13:51 -0500

    fixed the transition bug and it is the final commit

--- src/drum.js
+++ src/drum.js
@@ -1,19 +1,18 @@
-const keys = document.querySelectorAll('.key');
-// const playing = document.getElementById('playing');
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
 
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key ="${e.keyCode}"]`);
-    const key = document.querySelector(`audio[data-key ="${e.keyCode}"]`);
-    if (!audio) return; // stops the function
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
 
     key.classList.add('playing');
-    audio.currentTime = 0; // will automatically rewind
+    audio.currentTime = 0;
     audio.play();
 }
 
-function removeTransition (e) {
-    if (e.propertyName !== 'transform') return; // skip it if it is not a transform
-    this.classList.remove('playing');
-}
+const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
